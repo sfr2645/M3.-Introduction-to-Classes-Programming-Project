9f3a9fb26efb1f514f1fd601eb789f84f64a62cc
sfr2645 <sfr2645@email.vccs.edu>
2025-12-15 20:26:52 -0500

    include NumberArray

--- M3. Introduction to Classes Programming Project.cpp
+++ M3. Introduction to Classes Programming Project.cpp
@@ -1,40 +1,98 @@
 // M3. Introduction to Classes Programming Project.cpp
 //This is the class called date for the Number Array Class program 
 
-#ifndef NUMBERARRAY_H
-#define NUMBERARRAY_H
+#include "NumberArray.h"
+#include <iostream>
 
-class NumberArray
+using namespace std;
+
+// Initialize static constant
+const double NumberArray::DEFAULT_VALUE = 10000.0;
+
+// Constructor
+NumberArray::NumberArray(int s)
 {
-private:
-    int size;          // size of the array
-    double* numbers;   // pointer to dynamic array
+    if (s <= 0)
+        size = MAX_SIZE;
+    else
+        size = s;
 
-public:
-    // Named constants
-    static const int MAX_SIZE = 10;
-    static const double DEFAULT_VALUE;
+    numbers = new double[size];
 
-    // Constructor & Destructor
-    NumberArray(int size = MAX_SIZE);
-    ~NumberArray();
+    // Initialize array to 0
+    for (int i = 0; i < size; i++)
+        numbers[i] = 0.0;
+}
 
-    // Mutator
-    void setNumber(int index, double value);
+// Destructor
+NumberArray::~NumberArray()
+{
+    delete[] numbers;
+    cout << "The destructor is running" << endl;
+}
 
-    // Accessor
-    double getNumber(int index) const;
+// Store a number
+void NumberArray::setNumber(int index, double value)
+{
+    if (index < 0 || index >= size)
+    {
+        cout << "The index is out of the bounds of the array, number not stored" << endl;
+        return;
+    }
 
-    // Operations
-    double getMin() const;
-    double getMax() const;
-    double getAverage() const;
+    numbers[index] = value;
+}
 
-    // Print function (for testing)
-    void print() const;
+// Retrieve a number
+double NumberArray::getNumber(int index) const
+{
+    if (index < 0 || index >= size)
+    {
+        cout << "The Index is out of the bounds of the array, returning default" << endl;
+        return DEFAULT_VALUE;
+    }
 
-    // Accessor for size (inline)
-    int getSize() const { return size; }
-};
+    return numbers[index];
+}
 
-#endif
+// Minimum value
+double NumberArray::getMin() const
+{
+    double min = numbers[0];
+    for (int i = 1; i < size; i++)
+    {
+        if (numbers[i] < min)
+            min = numbers[i];
+    }
+    return min;
+}
+
+// Maximum value
+double NumberArray::getMax() const
+{
+    double max = numbers[0];
+    for (int i = 1; i < size; i++)
+    {
+        if (numbers[i] > max)
+            max = numbers[i];
+    }
+    return max;
+}
+
+// Average value
+double NumberArray::getAverage() const
+{
+    double sum = 0.0;
+    for (int i = 0; i < size; i++)
+        sum += numbers[i];
+
+    return sum / size;
+}
+
+// Print array
+void NumberArray::print() const
+{
+    for (int i = 0; i < size; i++)
+        cout << numbers[i] << " ";
+    cout << endl;
+}
