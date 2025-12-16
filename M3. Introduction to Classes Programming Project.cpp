bfd97b51f079568a5ee464d8e4784c32a7938ae5
sfr2645 <sfr2645@email.vccs.edu>
2025-12-15 19:29:26 -0500

    ,

--- M3. Introduction to Classes Programming Project.cpp
+++ M3. Introduction to Classes Programming Project.cpp
@@ -1,20 +1,47 @@
 // M3. Introduction to Classes Programming Project.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
 #include <iostream>
+#include "Date.h"
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    cout << "Test default constructor: ";
+    Date d1;
+    cout << d1.printNumeric() << endl << endl;
+
+    cout << "Test constructor with valid date: ";
+    Date d2(2, 28, 2009);
+    cout << d2.printNumeric() << endl << endl;
+
+    cout << "Test constructor with invalid month (45, 2, 2009): ";
+    Date d3(45, 2, 2009);
+    cout << d3.printNumeric() << endl << endl;
+
+    cout << "Test constructor with invalid day (2, 29, 2009): ";
+    Date d4(2, 29, 2009);
+    cout << d4.printNumeric() << endl << endl;
+
+    cout << "Test setDate with bad month (13): ";
+    d2.setDate(13, 10, 2009);
+    cout << d2.printNumeric() << endl << endl;
+
+    cout << "Test setDate with bad day (4, 31, 2009): ";
+    d2.setDate(4, 31, 2009);
+    cout << d2.printNumeric() << endl << endl;
+
+    cout << "Test for leap year with bad date (2/29/2009): ";
+    d2.setDate(2, 29, 2009);
+    cout << d2.printNumeric() << endl << endl;
+
+    cout << "Test for leap year with good date (2/29/2008): ";
+    Date d5(2, 29, 2008);
+    cout << d5.printNumeric() << endl << endl;
+
+    cout << "Test the print formats:" << endl;
+    cout << d5.printMonthFirst() << endl;
+    cout << d5.printDayFirst() << endl;
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
